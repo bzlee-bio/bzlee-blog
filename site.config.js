c13b68aa8db7ad0e0552f0d51070be4472ea9f2a
Byungjo Lee <blee.inf@gmail.com>
2024-01-15 10:07:14 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -3,7 +3,7 @@ const CONFIG = {
   profile: {
     name: "Byungjo Lee",
     image: "/profile.jpeg", 
-    role: "Researcher (BI+AI)",
+    role: "Researcher (Bioinformatics+Artificial intelligence)",
     bio: "",
     email: "blee.inf@gmail.com",
     linkedin: "",
